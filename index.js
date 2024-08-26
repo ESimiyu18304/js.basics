7dfc91f5e9cd2633fa0a3a610302a06eba8ccdb6
Elijah.Simiyu <elijahsimiyu18304@gmail.com>
2024-08-26 20:08:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let name ="Elijah";
+console.log(name);
+//variables 
+let firstName ="Elijah";
+let lastName = "Simiyu";
+let age = "23";
+console.log(firstName,lastName,age);
+//objects
+let person={
+name :'Elijah',
+age : 24
+};
+console.log(person);
+//Arrays
+let selectedColors = ['Red', 'Blue']
+console.log(selectedColors.length);
+//Functions
+function greet(name){
+    console.log('Hello'+ name)
+}
+
+greet('John');
